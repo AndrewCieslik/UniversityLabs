b8b4e4187abe6830bb710864a6aeb53744402667
AndrewCieslik <101130154+AndrewCieslik@users.noreply.github.com>
2022-05-10 22:37:01 +0200

    Add files via upload
    Zadanie 2.
    Plik tekstowy zawiera dane domów w postaci:
    NazwaDomu-CenaDomu-LiczbaKondygnacji-Lokalizacja
    Np.
    Chata-10000.00-1-Wies
    Willa-999999.99-4-Miasto
    Skopiuj do drugiego pliku tekstowego, parterowe domy wiejskie, zmniejszając ich cenę o 10%

---
+++ TextFilesExercise2.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class TextFilesExercise2 {
+    public static void main (String[] args) throws FileNotFoundException {
+        File fileRead = new File("Houses.csv");                       //read data from Houses.csv
+            try {
+                FileReader fileReader = new FileReader(fileRead);
+                BufferedReader bfReader = new BufferedReader(fileReader);
+                bfReader.lines().forEach(System.out::println);
+                bfReader.close();
+            }
+            catch (FileNotFoundException ex) {
+                System.err.println("File does not exist");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        Scanner sc = new Scanner(fileRead);
+        String rowReadFromFile;
+
+        File fileWrite = new File("OneStoreyHouses.csv");              //new csv for selected houses
+        PrintWriter writer = new PrintWriter(fileWrite);
+
+        for(int i = 0; sc.hasNext(); i++) {
+            rowReadFromFile = sc.next(); sc.nextLine();                         //read Houses.csv row by row
+
+            //only 1 storey houses in the countryside:
+            if (rowReadFromFile.split(",")[2].equals("1") && rowReadFromFile.split(",")[3].equals("Wies")) {
+
+                String price = rowReadFromFile.split(",")[1];             //house price from second column csv (String)
+                Double priceDouble = Double.parseDouble(price);                 //change String price to Double price
+                priceDouble -= 0.1*priceDouble;                                 // - 10% SELL
+
+                writer.println(rowReadFromFile.split(",")[0] + "," + priceDouble);      //write House name and sell price to new csv
+            }
+        }
+    writer.close();
+    sc.close();
+    }
+}
