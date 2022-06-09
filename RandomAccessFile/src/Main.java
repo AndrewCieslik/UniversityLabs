9de9b509ae0c5fc2494c5cfe2225b73ca32d2652
andrew <moto.cieslik@gmail.com>
2022-06-09 22:12:47 +0200

    RandomAccessFile correct

--- RandomAccessFile/src/Main.java
+++ RandomAccessFile/src/Main.java
@@ -1,5 +1,3 @@
-import java.io.EOFException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -31,40 +29,41 @@ public class Main {
             raf.seek(0);
             while (raf.getFilePointer() < raf.length()) {
 
+                System.out.println("Długość pliku: " + raf.length());
                 raf.readUTF();
-                System.out.println(raf.getFilePointer());
                 int czytanaKondygnacja = raf.readInt();
-                System.out.println(raf.getFilePointer());
-
                 long wskaznikCeny = raf.getFilePointer();
                 raf.readDouble();
-                System.out.println(raf.getFilePointer());
-
                 String czytanaLokalizacja = raf.readUTF();
-                System.out.println(raf.getFilePointer());
 
-                if (czytanaKondygnacja == 1 && czytanaLokalizacja.equals("Wies")) {
-                    raf.seek(wskaznikCeny);
-                    double czytanaCena = raf.readDouble();
-                    raf.seek(wskaznikCeny);
-                    raf.writeDouble(czytanaCena * 0.9);
-                }
+                    if (czytanaKondygnacja == 1 && czytanaLokalizacja.equals("Wies")) {
+                        raf.seek(wskaznikCeny);
+                        double czytanaCena = raf.readDouble();
+                        raf.seek(wskaznikCeny);
+                        raf.writeDouble(czytanaCena * 0.9);
+                        raf.readUTF();
+                        System.out.println("Wskaźnik na końcu pętli: " + raf.getFilePointer());
+                    }
             }
             raf.close();
+
         } catch (IOException e) {
             System.err.println("Błąd odczytu pliku");
         }
     }
 
     public static void main (String[] args) {
+
+        Parcela parcelaMiasto = new Parcela("Chata", 2, 70000, "Miasto");
+        parcelaMiasto.dopiszParcele();
+
+        Parcela parcelaNaWsi = new Parcela("Chata", 1, 90000, "Wies");
+        parcelaNaWsi.dopiszParcele();
+
         System.out.println("=== Przed zmianą cen ===");
         czytajPLik();
         System.out.println("========================");
 
-        Parcela parcelaMiasto = new Parcela("Chata", 2, 70000, "Miasto");
-        parcelaMiasto.dopiszParcele();
-        Parcela parcelaNaWsi = new Parcela("Chata", 1, 90000, "Wies");
-        parcelaNaWsi.dopiszParcele();
         zmienCene();
 
         System.out.println("=== Po zmianie cen ===");
